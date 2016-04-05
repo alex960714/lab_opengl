6b80820e4fac57885319c4080155a1db72f2bdd1
alex960714 <alex_9607@mail.ru>
2016-04-05 17:36:32 +0300

    First glControl properties update

--- lab_opengl/lab_opengl/Form1.Designer.cs
+++ lab_opengl/lab_opengl/Form1.Designer.cs
@@ -34,17 +34,19 @@
             // glControl1
             // 
             this.glControl1.BackColor = System.Drawing.Color.Black;
-            this.glControl1.Location = new System.Drawing.Point(33, 38);
+            this.glControl1.Location = new System.Drawing.Point(3, 3);
             this.glControl1.Name = "glControl1";
-            this.glControl1.Size = new System.Drawing.Size(150, 150);
+            this.glControl1.Size = new System.Drawing.Size(350, 283);
             this.glControl1.TabIndex = 0;
             this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(356, 291);
             this.Controls.Add(this.glControl1);
             this.Name = "Form1";
             this.Text = "Form1";
--- lab_opengl/lab_opengl/Form1.cs
+++ lab_opengl/lab_opengl/Form1.cs
@@ -14,14 +14,45 @@ namespace lab_opengl
 {
     public partial class Form1 : Form
     {
+        GLgraphics glgraphics = new GLgraphics();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            glgraphics.Setup(glControl1.Width, glControl1.Height);
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            glgraphics.Update();
+            glControl1.SwapBuffers();
         }
     }
 
     class GLgraphics
     {
+        public void Setup(int width, int height)
+        {
+            GL.ClearColor(Color.DarkGray);
+            GL.ShadeModel(ShadingModel.Smooth);
+            GL.Enable(EnableCap.DepthTest);
 
+            Matrix4 perspectiveMat =
+                Matrix4.CreatePerspectiveFieldOfView(
+                    MathHelper.PiOver4,
+                    width / (float)height, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspectiveMat);
+        }
+
+        public void Update()
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit |
+                ClearBufferMask.DepthBufferBit);
+        }
     }
 }
