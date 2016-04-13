e690e9aa3b6dde9ff4dabc5e227f680be31cc555
alex960714 <alex_9607@mail.ru>
2016-04-13 18:21:15 +0300

    Added WASD moving of sphere

--- lab_opengl/lab_opengl/Form1.cs
+++ lab_opengl/lab_opengl/Form1.cs
@@ -51,6 +51,25 @@ namespace lab_opengl
             while (glControl1.IsIdle)
                 glControl1.Refresh();
         }
+
+        private void glControl1_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void glControl1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.W)
+                glgraphics.dx += 0.1f;
+            if (e.KeyCode == Keys.S)
+                glgraphics.dx -= 0.1f;
+            if (e.KeyCode == Keys.A)
+                glgraphics.dy += 0.1f;
+            if (e.KeyCode == Keys.D)
+                glgraphics.dy -= 0.1f;
+
+        }
     }
 
     class GLgraphics
@@ -64,6 +83,11 @@ namespace lab_opengl
         public float radius = 5.385f;
 
         public float rotateAngle = 0;
+        public float rotateAngleX = 0;
+        public float rotateAngleY = 0;
+        public float dx = 0, dx1=0;
+        public float dy = 0, dy1=0;
+        public float dz = 0, dz1=0;
 
         public List<int> texturesIDs = new List<int>();
 
@@ -85,7 +109,8 @@ namespace lab_opengl
 
         public void Update()
         {
-            rotateAngle = rotateAngle + (float)0.1;
+            rotateAngleX = rotateAngleX + (float)((dx - dx1)*180/(0.5f*Math.PI));
+            rotateAngleY = rotateAngleY - (float)((dy - dy1) * 180 / (0.5f * Math.PI));
             GL.Clear(ClearBufferMask.ColorBufferBit |
                 ClearBufferMask.DepthBufferBit);
             Matrix4 viewMat = Matrix4.LookAt(
@@ -95,12 +120,17 @@ namespace lab_opengl
             GL.LoadMatrix(ref viewMat);
             Render();
 
-            cameraPosition = new Vector3(
+
+            cameraPosition = new Vector3(//dx+
                 (float)(radius * Math.Cos(Math.PI / 180.0f * latitude) *
-                Math.Cos(Math.PI / 180.0f * longitude)),
+                Math.Cos(Math.PI / 180.0f * longitude)),//dy+
                 (float)(radius * Math.Cos(Math.PI / 180.0f * latitude) *
-                Math.Sin(Math.PI / 180.0f * longitude)),
+                Math.Sin(Math.PI / 180.0f * longitude)),//0.3f+
                 (float)(radius * Math.Sin(Math.PI / 180.0f * latitude)));
+
+
+            dx1 = dx;
+            dy1 = dy;
 
         }
 
@@ -108,47 +138,63 @@ namespace lab_opengl
         {
             GL.Begin(PrimitiveType.Quads);
             GL.Color3(Color.Blue);
-            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 0.0f);
             GL.Color3(Color.Red);
-            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 0.0f);
             GL.Color3(Color.White);
-            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, 0.0f);
             GL.Color3(Color.Green);
-            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, 0.0f);
             GL.End();
         }
 
         public void Render()
         {
+            /*GL.PushMatrix();
+            GL.Translate(0, 0, 3);
+            GL.Rotate(10*rotateAngle, Vector3.UnitZ);
+            GL.Scale(1.0f, 1.5f, 2.5f);
+            GL.Color3(Color.LightSeaGreen);
             drawTriangle();
+            GL.PopMatrix();*/
 
             GL.PushMatrix();
-            GL.Translate(0, 0, -0.1f);
+            GL.Translate(0,0,0);
             //GL.Rotate(rotateAngle, Vector3.UnitZ);
-            GL.Scale(0.5f, 0.5f, 0.5f);
+            GL.Scale(2.5f, 2.5f, 2.5f);
             drawTestQuad();
             GL.PopMatrix();
 
-            GL.PushMatrix();
+            /*GL.PushMatrix();
             GL.Translate(1, 1, 1);
             GL.Rotate(rotateAngle, Vector3.UnitZ);
             GL.Scale(0.5f, 0.5f, 0.5f);
             drawTexturedQuad();
+            GL.PopMatrix();*/
+
+            GL.PushMatrix();
+            GL.Translate(dx, dy, 0.3f);
+            GL.Rotate(rotateAngleX, Vector3d.UnitY);
+            GL.Rotate(rotateAngleY, Vector3d.UnitX);
+            GL.Scale(0.5f, 0.5f, 0.5f);
+            GL.Color3(Color.BlueViolet);
+            drawTexturedSphere(0.5f, 50, 50);
             GL.PopMatrix();
 
             /*GL.PushMatrix();
-            GL.Translate(0, 0, 1);
-            GL.Scale(0.5f, 0.5f, 0.5f);*/
+            GL.Translate(5* Math.Sin(rotateAngle/2), 2* Math.Cos(rotateAngle/2), 0);
+            GL.Rotate(3*rotateAngle, Vector3.UnitZ);
+            GL.Scale(0.5f, 0.5f, 0.5f);
             GL.Color3(Color.BlueViolet);
-            drawSphere(2.0f, 20, 20);
-            //GL.PopMatrix();
+            drawTexturedSphere(2.0f, 50, 50);
+            GL.PopMatrix();
 
             GL.PushMatrix();
-            GL.Translate(0, 0, 3);
+            GL.Translate(0, 0, 3*Math.Sin(rotateAngle));
             GL.Scale(0.5f, 0.5f, 0.5f);
             GL.Color3(Color.Black);
             drawPoint();
-            GL.PopMatrix();
+            GL.PopMatrix();*/
             //drawTexturedQuad();
         }
 
@@ -203,8 +249,9 @@ namespace lab_opengl
             GL.Enable(EnableCap.Lighting);
             GL.Enable(EnableCap.Light0);
             GL.Enable(EnableCap.ColorMaterial);
+            GL.Enable(EnableCap.Light1);
 
-            Vector4 lightPosition = new Vector4(1.0f, 1.0f, 4.0f, 0.0f);
+            Vector4 lightPosition = new Vector4(3.1f, 2.0f, 6.0f, 0.0f);
             GL.Light(LightName.Light0, LightParameter.Position, lightPosition);
 
             Vector4 ambientColor = new Vector4(0.2f, 0.2f, 0.2f, 1.0f);
@@ -213,13 +260,50 @@ namespace lab_opengl
             Vector4 diffuseColor = new Vector4(0.6f, 0.6f, 0.6f, 1.0f);
             GL.Light(LightName.Light0, LightParameter.Diffuse, diffuseColor);
 
+            Vector4 lightPosition1 = new Vector4(3.0f, 2.0f, 6.0f, 0.0f);
+            GL.Light(LightName.Light1, LightParameter.Position, lightPosition1);
+
+            Vector4 ambientColor1 = new Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+            GL.Light(LightName.Light1, LightParameter.Ambient, ambientColor1);
+
+            Vector4 diffuseColor1 = new Vector4(0.8f, 0.8f, 0.8f, 1.0f);
+            GL.Light(LightName.Light1, LightParameter.Diffuse, diffuseColor1);
+
             Vector4 materialSpecular = new Vector4(1.0f, 1.0f, 1.0f, 1.0f);
             GL.Material(MaterialFace.Front, MaterialParameter.Specular, materialSpecular);
-            float materialShininess = 100;
+            float materialShininess = 1000;
             GL.Material(MaterialFace.Front, MaterialParameter.Shininess, materialShininess);
         }
 
         private void drawSphere(double r, int nx, int ny)
+        {
+            int ix, iy;
+            double x, y, z;
+            for (iy = 0; iy < ny; ++iy)
+            {
+                //GL.Enable(EnableCap.Texture2D);
+                //GL.BindTexture(TextureTarget.Texture2D, texturesIDs[0]);
+                GL.Begin(PrimitiveType.QuadStrip);
+                for (ix = 0; ix <= nx; ++ix)
+                {
+                    x = r * Math.Sin(iy * Math.PI / ny) * Math.Cos(2 * ix * Math.PI / nx);
+                    y = r * Math.Sin(iy * Math.PI / ny) * Math.Sin(2 * ix * Math.PI / nx);
+                    z = r * Math.Cos(iy * Math.PI / ny);
+                    GL.Normal3(x, y, z);
+                    GL.Vertex3(x, y, z);
+
+                    x = r * Math.Sin((iy + 1) * (Math.PI / ny)) * Math.Cos(2 * ix * Math.PI / nx);
+                    y = r * Math.Sin((iy + 1) * Math.PI / ny) * Math.Sin(2 * ix * Math.PI / nx);
+                    z = r * Math.Cos((iy + 1) * Math.PI / ny);
+                    GL.Normal3(x, y, z);
+                    GL.Vertex3(x, y, z);
+                }
+                GL.End();
+                //GL.Disable(EnableCap.Texture2D);
+            }
+        }
+
+        private void drawTexturedSphere(double r, int nx, int ny)
         {
             GL.Enable(EnableCap.Texture2D);
             GL.BindTexture(TextureTarget.Texture2D, texturesIDs[0]);
@@ -260,8 +344,9 @@ namespace lab_opengl
         private void drawTriangle()
         {
             GL.Begin(PrimitiveType.Triangles);
-            GL.Color3(Color.Green);
             GL.Vertex3(0.0f, 0.0f, 0.0f);
+            GL.Vertex3(1.0f, 0.0f, 0.0f);
+            GL.Vertex3(0.5f, 1.0f, 0.0f);
             GL.End();
         }
     }
